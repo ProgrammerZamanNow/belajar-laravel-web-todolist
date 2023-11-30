2a2dfaa96946358d8715da9716bcc58f7b57ed62
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-30 22:03:18 +0700

    todo service

--- app/Services/Impl/TodolistServiceImpl.php
+++ app/Services/Impl/TodolistServiceImpl.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Impl;
 
+use App\Models\Todo;
 use App\Services\TodolistService;
 use Illuminate\Support\Facades\Session;
 
@@ -10,32 +11,21 @@ class TodolistServiceImpl implements TodolistService
 
     public function saveTodo(string $id, string $todo): void
     {
-        if (!Session::exists("todolist")) {
-            Session::put("todolist", []);
-        }
-
-        Session::push("todolist", [
+        $todo = new Todo([
             "id" => $id,
             "todo" => $todo
         ]);
+        $todo->save();
     }
 
     public function getTodolist(): array
     {
-        return Session::get("todolist", []);
+        return Todo::query()->get()->toArray();
     }
 
     public function removeTodo(string $todoId)
     {
-        $todolist = Session::get("todolist");
-
-        foreach ($todolist as $index => $value) {
-            if ($value['id'] == $todoId) {
-                unset($todolist[$index]);
-                break;
-            }
-        }
-
-        Session::put("todolist", $todolist);
+        $todo = Todo::query()->find($todoId);
+        $todo->delete();
     }
 }
